31f893de986f66d3b70ceeb85d6108a043549635
Gabor <gabor.gyorgy@and.digital>
2020-09-01 17:40:13 +0100

    Configured sass

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,6 +1,6 @@
 import React, { FC, ReactElement } from "react";
 import { Link } from "react-router-dom";
-import './Home.css';
+import './Home.scss';
 
 const Home: FC = (): ReactElement => {
   return (
